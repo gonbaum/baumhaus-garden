8f0e3320ac8a927711bd40715a9310eac6c84f7e
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2022-01-03 19:59:13 +0100

    Add dockerfile

---
+++ pages/posts/first-steps.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+export default function FirstSteps() {
+  return (
+    <>
+      <h1>🪴 First Steps </h1>
+      <p>
+        After succesfully created my Gitlab project, I've proceeded to
+        containerize my app and setting up a Kubernetes cluster in GCP via
+        Gitlab CI/CD integration.
+      </p>
+      <p>
+        For containerize the app, I realised that we can use the example
+        Dockerfile from the official Next.js documentation. Once created the
+        file I've created the image and run a container locally to test it, by
+        exposing the port 3000. Commands used: docker build . -t
+        nextjs-baumhaus-garden docker run -p 3000:3000 nextjs-baumhaus-garden
+      </p>
+
+      <h3>📚 Useful resources: </h3>
+      <ul>
+        <li>
+          <a href="https://nextjs.org/docs/deployment">Next.js Deployment</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=Pd2tVxhFnO4">
+            Using Docker with Next.js (and Deploying with Google Cloud Run)
+          </a>
+        </li>
+        <li>
+          <a href="https://faun.pub/application-deployment-using-gitlab-ci-cd-on-managed-kubernetes-cluster-at-gcp-72b59496979c">
+            Create a Kubernetes cluster on GCP
+          </a>
+        </li>
+      </ul>
+      <h3>👞 Next steps:</h3>
+      <ul>
+        <li></li>
+      </ul>
+      <h2>
+        <Link href="/posts/the-origin">
+          <a>The origin</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
--- pages/posts/the-origin.js
+++ pages/posts/the-origin.js
@@ -1,22 +1,59 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function TheOrigin() {
   return (
     <>
-      <h1>The origin</h1>
+      <h1>🪴 The origin </h1>
       <p>
-          To kickstart this project, I've first created a repo in github and then bootstrapped a Next.js app locally by using the command:
-          npx create-next-app nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
-          taken from the offical documentation. 
+        To kickstart this project, I've first created a repo in github and then
+        bootstrapped a Next.js app locally by using the command: npx
+        create-next-app nextjs-blog --use-npm --example
+        "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
+        taken from the offical documentation.
       </p>
       <p>
-          Once running, I've created my first post (this one) and then proceeded to create a Gitlab project by importing my github repository dedicated to the project. I had to create a personal token for this and once this was done, I've also created a gitlab-ci.yml file with some 'mock pipeline' that tells if the push is made on the main branch or any other branch.
+        Once running, I've created my first post (this one) and then proceeded
+        to create a Gitlab project by importing my github repository dedicated
+        to the project. I had to create a personal token for this and once this
+        was done, I've also created a gitlab-ci.yml file with some 'mock
+        pipeline' that tells if the push is made on the main branch or any other
+        branch.
       </p>
+
+      <h3>📚 Useful articles: </h3>
+      <ul>
+        <li>
+          <a href="https://nextjs.org/learn/basics/create-nextjs-app">
+            Create a Next.js App
+          </a>
+        </li>
+        <li>
+          <a href="https://blog.eleven-labs.com/en/introduction-gitlab-ci/">
+            Introduction to Gitlab CI/CD
+          </a>
+        </li>
+        <li>
+          <a href="https://docs.gitlab.com/ee/ci/ci_cd_for_external_repos/github_integration.html">
+            Using GitLab CI/CD with a GitHub repository
+          </a>
+        </li>
+      </ul>
+      <Link href="/posts/first-steps"><h3>👞 Next steps:</h3></Link>
+      <ul>
+        <li>Containerize my Next.js app with Docker</li>
+        <li>Deploy and run my Next.js app in Google Cloud Platform</li>
+        <li>Automatize deployment process with Gitlab CI/CD</li>
+      </ul>
+      <h2>
+        <Link href="/posts/first-steps">
+          <a>Next steps</a>
+        </Link>
+      </h2>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
     </>
-  )
+  );
 }
